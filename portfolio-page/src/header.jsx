d29744ddf63a40d4cc695d575bedf5be1ba178be
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-17 23:14:15 -0700

    hw48

--- portfolio-page/src/header.jsx
+++ portfolio-page/src/header.jsx
@@ -1,10 +1,10 @@
-import sky from './skywebp'
+
 
 const Header = () => {
     return(
         <header>
             <h1>Dan Schechter</h1>
-            <img src={sky}/>
+           
         </header>
     )
 }
